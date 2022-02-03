64ef6486378ba06932321f37d882de081cb89fc0
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2022-02-04 08:51:18 +0900

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,18 +3,41 @@ from tqdm import tqdm
 from datasets import load_dataset, concatenate_datasets
 
 class DataLoader :
-    def __init__(self, data_size, seed) :
-        self.data_size = data_size
+    def __init__(self, seed) :
         self.seed = seed
-        self.prefix = 'sh110495/kor_newspaper'
 
     def load_data(self, api_key) :
         data_list = []
-        for i in range(self.data_size) :
-            dataset_name = self.prefix + str(i+1)
+
+        for i in range(10) :
+            dataset_name = 'sh110495/kor_newspaper' + str(i+1)
             dataset = load_dataset(dataset_name, use_auth_token=api_key)
-            data = dataset['train']
-            data_list.append(data)
+            dataset = dataset.remove_columns(['id', 'date', 'topic'])
+            data_list.append(dataset['train'])
+
+        for i in range(6) :
+            dataset_name = 'sh110495/kor-namuwiki' + str(i+1)
+            dataset = load_dataset(dataset_name, use_auth_token=api_key)
+            dataset = dataset.remove_columns(['title'])
+            data_list.append(dataset['train'])
+
+        for i in range(6) :
+            dataset_name = 'sh110495/kor-written-language' + str(i+1)
+            dataset = load_dataset(dataset_name, use_auth_token=api_key)
+            dataset = dataset.remove_columns(['id', 'date'])
+            data_list.append(dataset['train'])
+
+        dataset = load_dataset('sh110495/kor-wikipedia', use_auth_token=api_key)
+        dataset = dataset.remove_columns(['title'])
+        data_list.append(dataset['train'])
+
+        dataset = load_dataset('sh110495/kor-domain-language', use_auth_token=api_key)
+        dataset = dataset.remove_columns(['id', 'type', 'date', 'title'])
+        data_list.append(dataset['train'])
+
+        dataset = load_dataset('sh110495/kor-spoken-language', use_auth_token=api_key)
+        dataset = dataset.remove_columns(['title', 'publisher', 'date'])
+        data_list.append(dataset['train'])
 
         total_data = concatenate_datasets(data_list)
         total_data = total_data.shuffle(self.seed)
